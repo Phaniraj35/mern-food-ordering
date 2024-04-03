1a48abd2f91b388d17f06fc228a988bbc30953c6
phaniraj35 <phaniitng@gmail.com>
2024-04-03 20:24:17 +0530

    Added mobile app section

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,33 @@
+import LandingImage from '../assets/landing.png';
+import MobileAppImg from '../assets/appDownload.png'
+
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-12">
+
+        {/* Search */}
         <div className="flex flex-col gap-5 bg-white  shadow-md py-8 rounded-lg text-center -mt-16">
             <h1 className="text-3xl md:text-5xl font-bold tracking-tighter text-orange-600">
                 Tuck into a takeaway today
             </h1>
             <span className="text-normal md:text-xl tracking-tight">Food is just a click away!</span>
+        </div>
+
+        {/* Landing  */}
+        <div className="grid md:grid-cols-2 gap-5">
+            <img src={LandingImage} />
+
+            <div className="flex flex-col items-center justify-center text-center gap-4">
+                <span className='font-bold text-xl md:text-3xl tracking-tighter'>
+                    Order takeaway even faster!
+                </span>
+
+                <span>
+                    Download the mobile app for faster ordering and personalized recommendations
+                </span>
+
+                <img src={MobileAppImg} />
+            </div>
         </div>
     </div>
   )
