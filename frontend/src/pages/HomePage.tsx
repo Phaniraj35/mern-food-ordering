4d4ffbdd3d13fada7fa66282282bdaa741d23e00
phaniraj35 <phaniitng@gmail.com>
2024-04-03 19:23:56 +0530

    Implemented Nav,MobileNav, Hero Section

---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+  return (
+    <div className="flex flex-col gap-12">
+        <div className="flex flex-col gap-5 bg-white  shadow-md py-8 rounded-lg text-center -mt-16">
+            <h1 className="text-3xl md:text-5xl font-bold tracking-tighter text-orange-600">
+                Tuck into a takeaway today
+            </h1>
+            <span className="text-normal md:text-xl tracking-tight">Food is just a click away!</span>
+        </div>
+    </div>
+  )
+}
+
+export default HomePage
