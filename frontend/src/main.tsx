f0313d95753849ddb486d770315c388155469dfa
phaniraj35 <phaniitng@gmail.com>
2024-04-10 11:42:30 +0000

    implemented Error boundary in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client'
 import './global.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './AppRoutes.tsx'
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx';
+import { ErrorBoundary } from "react-error-boundary";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
-        <AppRoutes />
-      </Auth0ProviderWithNavigate>
+      <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+        <Auth0ProviderWithNavigate>
+          <AppRoutes />
+        </Auth0ProviderWithNavigate>
+      </ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>,
 )
